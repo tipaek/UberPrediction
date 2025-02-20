6586b27359c1e6b6da446ec2f5332fea6c381442
Timothy Paek <tax2farm@gmail.com>
2025-02-20 13:11:24 -0500

    training 1

--- ml/experiments/train_model.py
+++ ml/experiments/train_model.py
@@ -1,14 +1,36 @@
 # ml/experiments/train_model.py
-from comet_ml import Experiment
+from comet_ml import Experiment, start
 import pickle
 from ml.models.model import build_model
 from ml.data.load_data import load_training_data
 
+from ml.keys import API_KEY, PROJECT_NAME, WORKSPACE
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# For splitting the data and evaluating the model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# For the regression model
+from sklearn.ensemble import GradientBoostingRegressor
+
+# For saving the trained model
+import joblib
+
+data_path = "uber.csv"  
+df = pd.read_csv(data_path)
+
+df.head()
+
 # Initialize comet_ml experiment (replace with your actual API key and project details)
-experiment = Experiment(
-    api_key="YOUR_API_KEY",
-    project_name="ml-web-app",
-    workspace="your_workspace",
+experiment = start(
+  api_key=API_KEY,
+  project_name=PROJECT_NAME,
+  workspace=WORKSPACE
 )
 
 # Log hyperparameters
